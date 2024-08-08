443e9b9c127c1f129b2f55f0439480f795ea793f
unknown <ron.garcia.santos@gmail.com>
2024-08-08 19:27:53 +0800

    coin flip accomplished

--- coinFlipStreaks.py
+++ coinFlipStreaks.py
@@ -10,24 +10,28 @@
 import random
 numberOfStreaks = 0
 experimentLength = 10000
-coinFlipList = []
 headStreak = ['H', 'H', 'H', 'H', 'H', 'H']
 tailStreak = ['T', 'T', 'T', 'T', 'T', 'T']
 
 for experimentNumber in range(experimentLength):
  # Code that creates a list of 100 'heads' or 'tails' values.
+    coinFlipList = []
     for i in range(100):
         coinFlipList.append(random.choice(['H', 'T']))
  # Code that checks if there is a streak of 6 heads or tails in a row.
-# print('from this list: %s' % (coinFlipList))
-for x in range(len(coinFlipList)-5):
-    # print(coinFlipList[x:x+6], end=': ' )
-    # print(coinFlipList[x:x+6] == headStreak or coinFlipList[x:x+6] == tailStreak  )
-    if coinFlipList[x:x+6] == headStreak or coinFlipList[x:x+6] == tailStreak:
-        numberOfStreaks += 1
+    print('from this list: %s' % (coinFlipList))
+
+    for x in range(len(coinFlipList)-5):
+        # print(coinFlipList[x:x+6], end=': ' )
+        # print(coinFlipList[x:x+6] == headStreak or coinFlipList[x:x+6] == tailStreak  )
+        if coinFlipList[x:x+6] == headStreak or coinFlipList[x:x+6] == tailStreak:
+            numberOfStreaks += 1
+
+
      
 totalAttempts = experimentLength *  100
-print('Number of Streaks: %s' % (numberOfStreaks)) 
+print('Number of either Head or Tail Six Streaks: %s' % (numberOfStreaks)) 
 print('Total Attempts: %s' % totalAttempts)
 # print('Chance of streak: %s%%' % (numberOfStreaks / 100))   
-print('Chance of streak: %s%%' % (numberOfStreaks / (totalAttempts * 100) ))   
+print('Chance of streak: %s%%' % ( (numberOfStreaks / totalAttempts) * 100 ))   
+# print('Chance of streak: %s%%' % (numberOfStreaks / totalAttempts))
